4e78fa84ddff4761f2c2b5edb59e5f130c611bdd
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2017-09-29 10:05:55 +0500

    validation form fully complete

--- admin/admsettings/adm-settings.php
+++ admin/admsettings/adm-settings.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<style>
+		.err_span
+		{
+			color:red;
+
+		}
+		.err_span p
+		{
+			padding-top: 5px;
+		}
+		#form
+		{
+			margin-top: 10%;	
+		}
+	</style>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Profile settings</title>
@@ -15,12 +30,22 @@
 		<div class="row">
 			<form class="form-horizontal" id="form" action="">
 			  	<div class="form-group has-feedback nameBlock">
-				  <label class="control-label" for="name">Name</label>
-				  <input type="text" class="form-control" id="name" aria-describedby="inputSuccess2Status">
+				  <label class="control-label col-sm-2" for="name">Name</label>
+				  <div class="col-sm-6 for-span-name">
+				  	<input type="text" class="form-control" id="name" aria-describedby="inputSuccess2Status">
+				  </div>
+				  <div class="col-sm-2">
+				  	<span id="name-error" class = "err_span"><p></p></span>
+				  </div>
 			  	</div>
 				<div class="form-group has-feedback emailBlock">
-				  <label class="control-label" for="email">Email</label>
-				  <input type="text" class="form-control" id="email" aria-describedby="inputSuccess2Status">
+				  <label class="control-label col-sm-2" for="email">Email</label>
+				  <div class="col-sm-6 for-span-email">
+				  	<input type="text" class="form-control" id="email" aria-describedby="inputSuccess2Status">
+				  </div>
+				  <div class="col-sm-2" >
+				  	<span id="email-error" class = "err_span"><p></p></span>
+				  </div>
 			  	</div>
 			  	<div class="form-group"> 
 				    <div class="col-sm-offset-2 col-sm-10">
@@ -37,38 +62,69 @@
 	$(document).ready(function(){
 		var validName=false;
 		var validEmail = false;
+		$("#name-error").hide();
+		$("#email-error").hide();
+		$("#name").focusout(function()
+    {
+        check_name();
+    });
+
+	$("#email").focusout(function()
+	    {
+	        check_email();
+	    });
+
 
 		$("form").submit(function(event){
 			event.preventDefault(); // предотвратить отправку форму по умолчанию  
+			if(validEmail==true && validName==true)
+			{
+				$("form").unbind('submit').submit();//разрешить передачу формы
+
+			}
+		});
+
+		function check_name(){
 
 			var name = $("#name").val();
-			var email = $("#email").val();
-
 			if(name=="")
 			{
 				$("#name").parent().removeClass("has-success").addClass("has-error");
-				$(".nameBlock").append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+				$(".for-span-name").append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+				$("#name-error").html("Fill this field");
+				$("#name-error").show();
 				$('.glyphicon-ok').remove();
 				validName = false;
 			}else{
 				$("#name").parent().removeClass("has-error").addClass("has-success");
 				
-				$(".nameBlock").append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
-				$('.nameBlock .glyphicon-remove').remove();
+				$(".for-span-name").append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+				$('.for-span-name .glyphicon-remove').remove();
+				$("#name-error").hide();
 				validName = true;
 			}
+
+		}
+
+		function check_email(){
+			var email = $("#email").val();
+
+			
 			if(email == "")
 			{
 				
 				$("#email").parent().removeClass("has-success").addClass("has-error");
-				$(".emailBlock").append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+				$(".for-span-email").append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+				$("#email-error").html("Fill this field");
+				$("#email-error").show();
 				$('.glyphicon-ok').remove();
 				validEmail= false;
 			}else{
 				$("#email").parent().removeClass("has-error").addClass("has-success");
 				
-				$(".emailBlock").append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+				$(".for-span-email").append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
 				$('.emailBlock .glyphicon-remove').remove();
+				$("#email-error").hide();
 				validEmail=true;
 			}
 
@@ -77,33 +133,21 @@
             {
                $("#email").parent().removeClass("has-error").addClass("has-success");
 				
-				$(".emailBlock").append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
-				$('.emailBlock .glyphicon-remove').remove();
+				$(".for-span-email").append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+				$('.for-span-email .glyphicon-remove').remove();
+				$("#email-error").hide();
 				validEmail=true;
             }
         else
             {
-            	alert("Wrong email address");
                 $("#email").parent().removeClass("has-success").addClass("has-error");
-				$(".emailBlock").append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
-				$('.glyphicon-ok').remove();
+				$(".for-span-email").append('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+				$("#email-error").html("Wrong email address");
+				$("#email-error").show();
+				$('.emailBlock .glyphicon-ok').remove();
 				validEmail= false;
             }
 
-
-			if(validEmail==true && validName==true)
-			{
-				$("form").unbind('submit').submit();//разрешить передачу формы
-
-			}else{
-
-
-			}
-		});
-
-		function check_name(){
-
-			
 		}
 
 	});
