ce4398c23a18c8bc4bd6ab3bd2c51c5eca79a439
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2017-09-30 13:02:05 +0500

    submit-send

--- admin/admsettings/adm-settings.php
+++ admin/admsettings/adm-settings.php
@@ -49,7 +49,7 @@
 			  	</div>
 			  	<div class="form-group"> 
 				    <div class="col-sm-offset-2 col-sm-10">
-				      <button type="submit" class="btn btn-success">Submit</button>
+				      <button type="submit" class="btn btn-success">Send</button>
 				    </div>
 				</div>
 			</form>
